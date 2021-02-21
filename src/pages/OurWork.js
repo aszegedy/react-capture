2a9a3ab28925a136ecddd70265206110ddabc008
aszegedy <andras.szegedy@gmail.com>
2021-02-22 00:07:54 +0100

    11. Movie details

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -11,22 +11,22 @@ const OurWork = () => {
       <Movie>
         <h2>The Athlete</h2>
         <div className="line"></div>
-        <Link>
+        <Link to="work/the-athlete">
           <img src={athlete} alt="athlete" />
         </Link>
       </Movie>
       <Movie>
         <h2>The Racer</h2>
         <div className="line"></div>
-        <Link>
-          <img src={theracer} alt="athlete" />
+        <Link to="/work/the-racer">
+          <img src={theracer} alt="theracer" />
         </Link>
       </Movie>
       <Movie>
         <h2>Good Times</h2>
         <div className="line"></div>
-        <Link>
-          <img src={goodtimes} alt="athlete" />
+        <Link to="/work/good-times">
+          <img src={goodtimes} alt="goodtimes" />
         </Link>
       </Movie>
     </Work>
