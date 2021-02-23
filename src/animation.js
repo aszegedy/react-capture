c9ab93398613876ec84bd481944a35956e502c7b
aszegedy <andras.szegedy@gmail.com>
2021-02-23 17:46:26 +0100

    16 rest

--- src/animation.js
+++ src/animation.js
@@ -1,14 +1,14 @@
 export const pageAnimation = {
   hidden: {
     opacity: 0,
-    y: 300,
+    // y: -300,
   },
   show: {
     opacity: 1,
     y: 0,
     transition: {
       duration: 0.5,
-      when: "beforeChildren",
+
       staggerChildren: 0.25,
     },
   },
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -6,10 +6,14 @@ import home2 from "../img/home2.png";
 //Styles
 import { About, Description, Image, Hide } from "../styles";
 import styled from "styled-components";
+import { fade } from "../animation";
+
+import { useScroll } from "./useScroll";
 
 const ServicesSection = () => {
+  const [element, controls] = useScroll();
   return (
-    <Services>
+    <Services variants={fade} animate={controls} initial="hidden" ref={element}>
       <Description>
         <h2>
           High <span>quality</span> service.
@@ -65,6 +69,9 @@ const Services = styled(About)`
 const Cards = styled.div`
   display: flex;
   flex-wrap: wrap;
+  @media (max-width: 1300px) {
+    justify-content: center;
+  }
 `;
 
 const Card = styled.div`
